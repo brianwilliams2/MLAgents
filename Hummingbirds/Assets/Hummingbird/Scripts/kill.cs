034678b48327b588edb3914e5dc297d896bce05a
Braden Fullmer <46699133+bradenfullmer@users.noreply.github.com>
2020-10-14 12:00:14 -0600

    Small push
    Made a kill script for when the player collides into an agaent.
    
    Updated the lighting and skybox a bit to look more like night.

---
+++ Hummingbirds/Assets/Hummingbird/Scripts/kill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Enemy" && this.gameObject.tag == "Player"){
+            Destroy(other.gameObject);
+        }
+    }
+}
